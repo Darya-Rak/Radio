5cf918fb0ee49a6841da54572f334ce255d3e21e
Darya-Rak <Darya-Rak@users.noreply.github.com>
2021-03-07 13:44:05 +0300

    added RadioTest

---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+    Radio radio = new Radio();
+
+    @Test
+    public void createRadio() {
+        Radio radio = new Radio();
+        System.out.println("hello");
+    }
+
+}
