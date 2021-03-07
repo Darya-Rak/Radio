cb24e980a6481d21461756365fb9350fbb10bee9
Darya-Rak <Darya-Rak@users.noreply.github.com>
2021-03-07 13:36:38 +0300

    added Radio

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Radio {
+    String name;
+    int maxVolume = 10;
+    int minVolume = 0;
+    int currentVolume;
+
+    private int currentRadioStation;
+    private int maxRadioStation = 9;
+    private int minRadioStation = 0;
+}
+
